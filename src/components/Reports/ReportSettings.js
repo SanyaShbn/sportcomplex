2597bdf5d8a227ffa3ef2ef28575a4d55d4da30c
SanyaShbn <144464447+SanyaShbn@users.noreply.github.com>
2024-06-02 11:59:05 +0300

    Deploying on Netlify. 7th try

--- src/components/Reports/ReportSettings.js
+++ src/components/Reports/ReportSettings.js
@@ -39,20 +39,20 @@ const ReportForm = ({ setSelectedButtonLink, link }) => {
   return (
     <form onSubmit={handleSubmit}>
     <Stack direction="row" spacing={2} mt={1}>
-      <TextField name="title" label="Заголовок" value={current_data.title !== '' && typeof current_data.title !== 'undefined' ? 
+      <TextField name="title" label="Заголовок" value={data.title !== '' && typeof data.title !== 'undefined' ? 
       data.title : ''} required onChange={handleChange} />
-      <TextField name="subject" label="Тема" value={current_data.subject !== '' && typeof current_data.subject !== 'undefined' ? 
+      <TextField name="subject" label="Тема" value={data.subject !== '' && typeof data.subject !== 'undefined' ? 
       data.subject : ''} required onChange={handleChange} />
     </Stack>
     <Stack mt={3}>
-      <TextField name="textContent" label="Текстовое содержание отчета" multiline maxRows={50}  value={current_data.textContent !== '' && 
-      typeof current_data.textContent!== 'undefined' ? data.textContent : ''} onChange={handleChange} />
+      <TextField name="textContent" label="Текстовое содержание отчета" multiline maxRows={50}  value={data.textContent !== '' && 
+      typeof data.textContent!== 'undefined' ? data.textContent : ''} onChange={handleChange} />
     </Stack>
     <Stack direction="row" spacing={2} mt={1}>
     </Stack>
     <FormControl component="fieldset">
       <FormLabel >Табличные данные</FormLabel>
-        <RadioGroup name="option" value={current_data.option  !== '' && typeof current_data.option !== 'undefined' ? 
+        <RadioGroup name="option" value={data.option  !== '' && typeof data.option !== 'undefined' ? 
         data.option : ''} onChange={handleChange}>
         <Grid container>
         <Grid item xs={xs} sm={sm}>
